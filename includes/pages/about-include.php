af624381f7eb717b77a0da60de7a670815818011
wictorstenseke <wictor.johansson@gmail.com>
2017-03-22 14:58:03 +0100

    added and update on about and error

---
+++ includes/pages/about-include.php
@@ -0,0 +1,12 @@
+    <!-- change the header here -->
+    <div class="container-fluid">
+        <div class="row">
+            <!-- navy-blue header -->
+            <div class="col-xs-12 text-center banner-header">
+                <h1>About Page</h1>
+                <a href="/" class="header-cta btn btn-lg btn-success" role="button">Tillbaka</a>
+            </div>
+        </div><!-- /row -->
+    </div><!-- /header-container -->
+
+
---
+++ includes/pages/error_404-include.php
@@ -0,0 +1,16 @@
+    <!-- change the header here -->
+    <div class="container-fluid">
+        <div class="row">
+            <!-- header -->
+            <div class="col-xs-12 text-center error-404">
+                <h1>Sorry...</h1>
+                <p>Det verkar som att vi skickat dig fel. Klicka på knappen för att återvända till startsidan.</p>
+                <h2>🙌🏻</h2>
+                <a href="/" class="header-cta btn btn-lg btn-success" role="button">
+                <i class="fa fa-home" aria-hidden="true"></i>&nbsp;&nbsp;Gå till startsidan
+                </a>
+            </div>
+        </div><!-- /row -->
+    </div><!-- /header-container -->
+
+
